610fa91430253400d43670ff1b41e24775d42301
neurall <neuralll@gmail.com>
2016-07-03 09:25:33 +0200

    Revert "Fixed missing plugin reference"
    This reverts commit cecc2ddbf82d3b1b8786e3c37403427b7433bd54.

--- src/index.js
+++ src/index.js
@@ -13,11 +13,8 @@ import "./tokenizer/context";
 
 import flowPlugin from "./plugins/flow";
 import jsxPlugin from "./plugins/jsx";
-import jadexPlugin from "./plugins/jadex";
-
 plugins.flow = flowPlugin;
 plugins.jsx = jsxPlugin;
-plugins.jadex = jadexPlugin;
 
 export function parse(input, options) {
   return new Parser(options, input).parse();
